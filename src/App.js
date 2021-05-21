0a100554c03177a1a1c1438ab80ecabe31364267
NAZMUS SAKIB <sakibabir52@gmail.com>
2021-05-21 12:47:55 +0600

    Hello doctor deployed in github

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { createContext, useState } from 'react';
 import './App.css';
 import {
   BrowserRouter as Router,
@@ -7,16 +7,43 @@ import {
   Link
 } from "react-router-dom";
 import Home from './components/Home/Home/Home';
+import Appointment from './components/Appointment/Appointment/Appointment';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Login from './components/Login/Login';
+import Dashboard from './components/Dashboard/Dashboard/Dashboard';
+import AllPatients from './components/AllPatients/AllPatients/AllPatients';
+import AddDoctor from './components/AddDoctor/AddDoctor';
+import PrivateRoute from './components/PrivateRoute/PrivateRoute';
+
+export const UserContext = createContext();
 
 function App() {
+  const [loggedInUser, setLoggedInUser] = useState({});
   return (
+    <UserContext.Provider value={[loggedInUser, setLoggedInUser]}>
     <Router>
       <Switch>
         <Route exact path="/">
           <Home/>
         </Route>
+        <Route path="/appointment">
+          <Appointment/>
+        </Route>
+        <PrivateRoute path="/dashboard">
+          <Dashboard/>
+        </PrivateRoute>
+          <Route path="/allPatients">
+            <AllPatients></AllPatients>
+          </Route>
+          <Route path="/addDoctor">
+            <AddDoctor></AddDoctor>
+          </Route>
+        <Route path="/login">
+          <Login/>
+        </Route>
       </Switch>
     </Router>
+    </UserContext.Provider>
   );
 }
 
---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAK1yO6img3nx20be9IzRVXFROdEJRTTaE",
+  authDomain: "hello-doctor1.firebaseapp.com",
+  projectId: "hello-doctor1",
+  storageBucket: "hello-doctor1.appspot.com",
+  messagingSenderId: "198334442410",
+  appId: "1:198334442410:web:732d955e509ac5381f99d3"
+};
+
+export default firebaseConfig;
